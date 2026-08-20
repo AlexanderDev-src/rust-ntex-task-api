9145e659f7ca966e67f24abef3a31a3e8741ad8e
Alexander <patcharapol.pantawee@gmail.com>
2026-08-20 14:39:20 +0700

    feat: add GET /health endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/health", get(health));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+async fn health() -> &'static str {
+    "OK"
 }
