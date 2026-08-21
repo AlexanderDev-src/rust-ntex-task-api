4e95c829dbaf0b49ad3ae51338f9df9c452bcb33
Alexander <patcharapol.pantawee@gmail.com>
2026-08-21 09:15:33 +0700

    Learn: ntex

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
-use axum::{Router, routing::get};
+use ntex::web;
 
-#[tokio::main]
-async fn main() {
-    let app = Router::new().route("/health", get(health));
+#[ntex::main]
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
-    axum::serve(listener, app).await.unwrap();
+async fn main() -> std::io::Result<()> {
+    web::HttpServer::new(async || {
+        web::App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
-async fn health() -> &'static str {
-    "OK"
+#[web::get("/")]
+async fn hello() -> impl web::Responder {
+    web::HttpResponse::Ok().body("Hello world!")
+}
+
+#[web::post("/echo")]
+async fn echo(req_body: String) -> impl web::Responder {
+    web::HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl web::Responder {
+    web::HttpResponse::Ok().body("Hey there!")
 }
